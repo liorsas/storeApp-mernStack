7ea7e2e5a7718319224bf9f8c9e9345ba462f52b
liorsas <85841112+liorsas@users.noreply.github.com>
2021-12-28 10:34:22 +0200

    second commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var cors = require('cors');
 const { Mongoose } = require('mongoose');
 let app = express();
 app.use(cors())
-
+process.env.NODE_ENV = "production"
 
 const PORT = process.env.PORT || 3001
 
@@ -22,7 +22,19 @@ app.use('/api/product', productRouter);
 app.use('/api/customers', customerRouter);
 app.use('/api/purchases', purchaseRouter);
 
+ if(process.env.NODE_ENV === 'production'){
+ app.use(express.static(path.join((__dirname,'client/build'))))
 
+ app.get('*',(req,res) => {
+     res.sendFile(path.join(__dirname,'client','build','index.html'))
+ })
+ }
+ else {
+   app.get('/', (req,res) => {
+
+    res.send('api running')
+   }) 
+ }
 
 
 
